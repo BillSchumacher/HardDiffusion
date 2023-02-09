9ffe4be9090e9d29c84fe342f2768cf446e58d88
BillSchumacher <34168009+BillSchumacher@users.noreply.github.com>
2023-02-08 21:07:45 -0600

    Fix linting error in settings.

--- HardDiffusion/settings.py
+++ HardDiffusion/settings.py
@@ -89,20 +89,15 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
-
+AUTH_PASSWORD_CLASSES = [
+    'UserAttributeSimilarityValidator',
+    'MinimumLengthValidator',
+    'CommonPasswordValidator',
+    'NumericPasswordValidator'
+]
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {'NAME': f"django.contrib.auth.password_validation.{clazz}"}
+    for clazz in AUTH_PASSWORD_CLASSES
 ]
 
 
