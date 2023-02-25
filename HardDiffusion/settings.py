f9ac0a1d07033dee48bc437cc8c757667ebc0cd2
BillSchumacher <34168009+BillSchumacher@users.noreply.github.com>
2023-02-25 01:19:53 -0600

    Fix media path, add sentry and analytics.

--- HardDiffusion/settings.py
+++ HardDiffusion/settings.py
@@ -17,10 +17,28 @@ import sys
 from pathlib import Path
 
 from dotenv import load_dotenv
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
 
 load_dotenv()
 os.environ.setdefault('FORKED_BY_MULTIPROCESSING', '1')
 
+sentry_sdk.init(
+    dsn="https://33009b0658104b1ba13167fd3c56da0d@o348660.ingest.sentry.io/4504739432693760",
+    integrations=[
+        DjangoIntegration(),
+    ],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=0.01,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=False
+)
 
 hostname = os.getenv(
     'HOSTNAME',
