cc0f065637764f334b3fe1309f3318d6945aefc3
BillSchumacher <34168009+BillSchumacher@users.noreply.github.com>
2023-03-04 06:04:30 -0600

    Add copy to clipboard support for images.

--- hard_diffusion/lib/generated_images.dart
+++ hard_diffusion/lib/generated_images.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
 import 'dart:convert';
+import 'dart:ui';
+import 'package:flutter/services.dart';
 import 'package:hard_diffusion/api/images/generated_image.dart';
 import 'package:hard_diffusion/exception_indicators/empty_list_indicator.dart';
 import 'package:hard_diffusion/exception_indicators/error_indicator.dart';
@@ -8,6 +10,7 @@ import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:provider/provider.dart';
+import 'package:super_clipboard/super_clipboard.dart';
 
 /*
 class GeneratedImages extends StatelessWidget {
@@ -76,6 +79,7 @@ class _ImageDetailsState extends State<ImageDetails> {
   _ImageDetailsState({required this.item});
   final GeneratedImage item;
   bool showEditButton = false;
+  Uint8List imageBytes = Uint8List(0);
 
   @override
   Widget build(BuildContext context) {
@@ -90,6 +94,7 @@ class _ImageDetailsState extends State<ImageDetails> {
     } else if (item.generatedAt == null) {
       if (previewImage != null && previewImage.isNotEmpty) {
         final decodedBytes = base64Decode(previewImage);
+        imageBytes = decodedBytes;
         image = Image.memory(decodedBytes);
       } else {
         image = paintingImage;
@@ -100,15 +105,32 @@ class _ImageDetailsState extends State<ImageDetails> {
       image = CachedNetworkImage(
         placeholder: (context, url) => const CircularProgressIndicator(),
         imageUrl: "http://localhost:8000/${item.filename}",
-        imageBuilder: (context, imageProvider) => Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: imageProvider,
-              fit: BoxFit.fill,
-              //colorFilter: ColorFilter.mode(Colors.red, BlendMode.colorBurn)),
-            ),
-          ),
-        ),
+        imageBuilder: (context, imageProvider) {
+          imageProvider
+              .obtainKey(createLocalImageConfiguration(context))
+              .then((value) {
+            imageProvider.load(value, (bytes,
+                {allowUpscaling: true,
+                cacheHeight: 200,
+                cacheWidth: 200}) async {
+              imageBytes = bytes.buffer.asUint8List();
+              return instantiateImageCodec(imageBytes);
+            });
+            return Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: imageProvider,
+                  fit: BoxFit.fill,
+                  //colorFilter: ColorFilter.mode(Colors.red, BlendMode.colorBurn)),
+                ),
+              ),
+            );
+          });
+
+          return Image(
+            image: imageProvider,
+          );
+        },
         errorWidget: (context, url, error) => errorImage,
       );
     }
@@ -141,7 +163,11 @@ class _ImageDetailsState extends State<ImageDetails> {
             left: 5,
             child: ElevatedButton(
               child: Text('Copy'),
-              onPressed: () => {},
+              onPressed: () async {
+                final item = DataWriterItem();
+                item.add(Formats.png(imageBytes));
+                await ClipboardWriter.instance.write([item]);
+              },
             ),
           ),
           Positioned(
